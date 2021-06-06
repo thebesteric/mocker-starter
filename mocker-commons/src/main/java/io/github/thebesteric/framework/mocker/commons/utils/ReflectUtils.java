9f8f129ba97f61d589c2afde51495337a355f25e
eric <whatisjava@hotmail.com>
2021-06-07 01:14:13 +0800

    update

--- mocker-commons/src/main/java/io/github/thebesteric/framework/mocker/commons/utils/ReflectUtils.java
+++ mocker-commons/src/main/java/io/github/thebesteric/framework/mocker/commons/utils/ReflectUtils.java
@@ -1,10 +1,17 @@
 package io.github.thebesteric.framework.mocker.commons.utils;
 
+import org.springframework.util.ClassUtils;
+
+import java.io.File;
+import java.io.IOException;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.*;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.net.JarURLConnection;
+import java.net.URL;
+import java.util.*;
+import java.util.function.Consumer;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
 
 /**
  * ReflectUtils
@@ -15,6 +22,8 @@ import java.util.List;
  * @since 1.0
  */
 public class ReflectUtils {
+
+    public static final String[] COMPILE_PATHS = new String[]{"target\\classes", "build\\classes\\java\\main"};
 
     public static String[] getModifiers(Method method) {
         return Modifier.toString(method.getModifiers()).split(" ");
@@ -116,6 +125,19 @@ public class ReflectUtils {
         return constructors.get(0);
     }
 
+    public static String getProjectPath() {
+        ClassLoader defaultClassLoader = ClassUtils.getDefaultClassLoader();
+        if (defaultClassLoader != null) {
+            String path = Objects.requireNonNull(defaultClassLoader.getResource("")).getPath();
+            if (System.getProperty("os.name").toLowerCase().contains("windows")) {
+                path = path.substring(1);
+            }
+            return path.replaceAll("%20", " ").replace("target/classes/", "");
+        }
+        return null;
+    }
+
+
     public static Object newInstance(Constructor<?> constructor) throws Throwable {
         constructor.setAccessible(true);
         Parameter[] parameters = constructor.getParameters();
@@ -150,6 +172,110 @@ public class ReflectUtils {
             return argumentValues;
         }
         return null;
+    }
+
+    public static List<Class<?>> getInterfaceImpls(Class<?> interfaceClass) {
+        return getInterfaceImpls(interfaceClass, COMPILE_PATHS);
+
+    }
+
+    public static List<Class<?>> getInterfaceImpls(Class<?> interfaceClass, String[] compilePaths) {
+        ArrayList<Class<?>> implClasses = new ArrayList<>();
+        if (!interfaceClass.isInterface()) {
+            throw new RuntimeException(interfaceClass.getName() + " is not a interface");
+        }
+        scan(getProjectPath(), compilePaths, (className) -> {
+            try {
+                Class<?> clazz = Class.forName(className);
+                if (!clazz.isInterface() && clazz.getSuperclass() != null) {
+                    Set<Class<?>> interfaces = new HashSet<>(Arrays.asList(clazz.getInterfaces()));
+                    if (interfaces.contains(interfaceClass)) {
+                        implClasses.add(Class.forName(clazz.getName()));
+                    }
+                }
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            }
+        });
+        return implClasses;
+
+    }
+
+    public static void scan(String projectPath, Consumer<String> consumer) {
+        scan(projectPath, COMPILE_PATHS, consumer);
+    }
+
+    public static void scan(String projectPath, String[] compilePaths, Consumer<String> consumer) {
+        URL url = Thread.currentThread().getContextClassLoader().getResource("");
+        if (url != null) {
+            if ("file".equals(url.getProtocol())) {
+                doScan(new File(projectPath + "/"), compilePaths, consumer);
+            } else if ("jar".equals(url.getProtocol())) {
+                try {
+                    JarURLConnection connection = (JarURLConnection) url.openConnection();
+                    JarFile jarFile = connection.getJarFile();
+                    Enumeration<JarEntry> jarEntries = jarFile.entries();
+                    while (jarEntries.hasMoreElements()) {
+                        JarEntry jar = jarEntries.nextElement();
+                        if (jar.isDirectory() || !jar.getName().endsWith(".class")) {
+                            continue;
+                        }
+                        String jarName = jar.getName();
+                        String classPath = jarName.replaceAll("/", ".");
+                        String className = classPath.substring(0, classPath.lastIndexOf("."));
+                        consumer.accept(className);
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    public static void doScan(File file, Consumer<String> consumer) {
+        doScan(file, COMPILE_PATHS, consumer);
+    }
+
+    public static void doScan(File file, String[] compilePaths, Consumer<String> consumer) {
+        if (file.isDirectory()) {
+            for (File _file : Objects.requireNonNull(file.listFiles())) {
+                doScan(_file, compilePaths, consumer);
+            }
+        } else {
+            String filePath = file.getPath();
+            int index = filePath.lastIndexOf(".");
+            if (index != -1 && ".class".equals(filePath.substring(index))) {
+                filePath = extractLegalFilePath(filePath, compilePaths);
+                if (filePath != null) {
+                    String classPath = filePath.replaceAll("\\\\", ".");
+                    String className = classPath.substring(0, classPath.lastIndexOf("."));
+                    consumer.accept(className);
+                }
+            }
+        }
+    }
+
+    private static String extractLegalFilePath(String filePath, String[] compilePaths) {
+        for (String compilePath : compilePaths) {
+            int index = filePath.indexOf(compilePath);
+            if (index != -1) {
+                return filePath.substring(index + compilePath.length() + 1);
+            }
+        }
+        return null;
+    }
+
+    public static String getMethodSignature(Method method) {
+        return method.getDeclaringClass().getName() + "." + method.getName();
+    }
+
+    public static String getMethodParametersSignature(Method method) {
+        StringBuilder signature = new StringBuilder();
+        Class<?>[] parameterTypes = method.getParameterTypes();
+        for (Class<?> parameterType : parameterTypes) {
+            signature.append(parameterType.getName()).append(",");
+        }
+        return signature.length() > 0 ? signature.substring(0, signature.length() - 1) : "";
     }
 
 
