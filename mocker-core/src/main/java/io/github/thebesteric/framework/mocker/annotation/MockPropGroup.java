d527556236dd27463246050cdd5aca538ad446e9
eric <whatisjava@hotmail.com>
2021-07-11 16:23:52 +0800

    feat: Support for internal loops

---
+++ mocker-core/src/main/java/io/github/thebesteric/framework/mocker/annotation/MockPropGroup.java
@@ -0,0 +1,11 @@
+package io.github.thebesteric.framework.mocker.annotation;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface MockPropGroup {
+    String[] value() default {};
+}
