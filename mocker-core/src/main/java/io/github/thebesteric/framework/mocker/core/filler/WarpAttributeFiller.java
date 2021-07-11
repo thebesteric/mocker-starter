a621fc7dda02ad16c52eb87da8da58ea1db8d035
eric <whatisjava@hotmail.com>
2021-07-12 00:33:05 +0800

    fix: Fixed an issue with wrapper types handling null values

--- mocker-core/src/main/java/io/github/thebesteric/framework/mocker/core/filler/WarpAttributeFiller.java
+++ mocker-core/src/main/java/io/github/thebesteric/framework/mocker/core/filler/WarpAttributeFiller.java
@@ -30,23 +30,25 @@ public class WarpAttributeFiller extends AbstractAttributeFiller {
 
     @Override
     public Object doMockValue(Class<?> fieldClassType, Object instance, String value) {
-        if (fieldClassType == Character.class) {
-            return value != null ? value.charAt(0) : '0';
-        } else if (fieldClassType == Byte.class) {
-            return value != null ? value.getBytes(StandardCharsets.UTF_8)[0] : RANDOM.nextInt() + 1;
-        } else if (fieldClassType == Short.class) {
-            return value != null ? Short.parseShort(value) : RANDOM.nextInt(100) + 1;
-        } else if (fieldClassType == Integer.class) {
-            return value != null ? Integer.parseInt(value) : RANDOM.nextInt();
-        } else if (fieldClassType == Long.class) {
-            return value != null ? Long.parseLong(value) : RANDOM.nextLong();
-        } else if (fieldClassType == Float.class) {
-            return value != null ? Float.parseFloat(value) : RANDOM.nextFloat() * 100;
-        } else if (fieldClassType == Double.class) {
-            return value != null ? Double.parseDouble(value) : RANDOM.nextDouble() * 100;
-        } else if (fieldClassType == Boolean.class) {
-            return value != null ? Boolean.parseBoolean(value) : RANDOM.nextBoolean();
+        if (!NULL_VALUE.equals(value)) {
+            if (fieldClassType == Character.class) {
+                return value != null ? value.charAt(0) : '0';
+            } else if (fieldClassType == Byte.class) {
+                return value != null ? value.getBytes(StandardCharsets.UTF_8)[0] : RANDOM.nextInt() + 1;
+            } else if (fieldClassType == Short.class) {
+                return value != null ? Short.parseShort(value) : RANDOM.nextInt(100) + 1;
+            } else if (fieldClassType == Integer.class) {
+                return value != null ? Integer.parseInt(value) : RANDOM.nextInt();
+            } else if (fieldClassType == Long.class) {
+                return value != null ? Long.parseLong(value) : RANDOM.nextLong();
+            } else if (fieldClassType == Float.class) {
+                return value != null ? Float.parseFloat(value) : RANDOM.nextFloat() * 100;
+            } else if (fieldClassType == Double.class) {
+                return value != null ? Double.parseDouble(value) : RANDOM.nextDouble() * 100;
+            } else if (fieldClassType == Boolean.class) {
+                return value != null ? Boolean.parseBoolean(value) : RANDOM.nextBoolean();
+            }
         }
-        return value;
+        return value.equals(NULL_VALUE) ? null : value;
     }
 }
